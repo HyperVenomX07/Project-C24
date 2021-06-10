83c7a9f7959163d4d90c98e970408a2ec2b96883
HyperVenomX07 <78355144+HyperVenomX07@users.noreply.github.com>
2021-06-11 00:39:39 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,41 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var hammer;
+var ball;
+var stone;
+var iron;
+
+
+function setup(){
+    var canvas = createCanvas(1200,600);
+    engine = Engine.create();
+    world = engine.world;
+
+    plane = new Plane(600,height,1200,20)
+    hammer = new Hammer(10,100);
+    ball=new Rubber(400,500,50)
+    stone=new Stone(600,400)
+    iron=new Iron(700,500)
+  
+
+
+}
+
+function draw(){
+    background("lightBlue");
+    Engine.update(engine);
+
+
+    plane.display();
+    hammer.display();
+    ball.display();
+    stone.display();
+    iron.display();
+
+
+    
+ 
+}
